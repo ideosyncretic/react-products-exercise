d4b16105d495d64e5258dd6507a97adf800ace45
Sprazzeus <sprazzeus@gmail.com>
2016-09-20 00:12:40 +0800

    added hashHistory from react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, IndexLink, Link} from 'react-router';
+import { Router, Route, IndexRoute, IndexLink, Link, hashHistory} from 'react-router';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
     </Route>
   </Router>,
