d16483ce8589120175fc723d680de9b6ea2b0cc9
Sprazzeus <sprazzeus@gmail.com>
2016-09-19 21:50:10 +0800

    set up app frame in App.js

--- products-app/src/App.js
+++ products-app/src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      
+      <div className="page">
+        <nav>
+          <Link to ="/" activeClassName="active"><button>Browse</button></Link>
+          <Link to ="/cart" activeClassName="active"><button>Cart</button></Link>
+        </nav>
+
+        <div className="main">
+        </div>
+      </div>
     );
   }
 }
