10c1f1c64f1970fa82e0a40aa3f1a5fb39dae1d2
Sprazzeus <sprazzeus@gmail.com>
2016-09-19 22:47:04 +0800

    added react router

--- products-app/src/index.js
+++ products-app/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router'
 import App from './App';
 import './index.css';
 
+var {
+  Router,
+  Route,
+  IndexRoute,
+  IndexLink,
+  Link
+} = ReactRouter
+
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
