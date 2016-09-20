e7b93b0c1f2af53626955ebb10837f2a911915a8
Sprazzeus <sprazzeus@gmail.com>
2016-09-20 20:03:58 +0800

    set up filter components with sample data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Route, IndexRoute, IndexLink, Link, hashHistory} from 'react-router';
 import './App.css';
+import dataSample from './products.json'
 
 class App extends Component {
   render() {
@@ -12,10 +13,65 @@ class App extends Component {
         </nav>
 
         <div className="main">
+          {this.props.children}
         </div>
       </div>
     );
   }
 }
 
-export default App;
+const Browse = React.createClass({
+  render () {
+    return (
+      <div>
+        <ProductFilterBox data={dataSample} />
+        <FilterableProductList />
+      </div>
+    )
+  }
+})
+
+const FilterableProductList = React.createClass({
+  render () {
+    return (
+      <div className="filterable-products-list">
+      </div>
+    )
+  }
+})
+
+const ProductFilterBox = React.createClass({
+  render () {
+    return (
+      <ProductFilterList data={this.props.data} />
+    )
+  }
+})
+
+const ProductFilterList = React.createClass({
+  render: function() {
+    var productFilterCategory = this.props.data.filters.map(function(filter) {
+      return (
+        <ul>
+          <h4>{filter.name}</h4>
+          {filter.values.map(function(value) {
+            return (
+              <li>
+                <label for={"option" + value}>
+                  <input id={"option" + value} type="checkbox" name="field" value="option"/>{value}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      );
+    });
+    return (
+      <form className="productFilterList">
+        {productFilterCategory}
+      </form>
+    );
+  }
+});
+
+export {App, Browse};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, IndexLink, Link, hashHistory} from 'react-router';
-import App from './App';
+import {App, Browse} from './App';
 import './index.css';
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Browse}>
+      </IndexRoute>
     </Route>
   </Router>,
   document.getElementById('root')
