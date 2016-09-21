8eef5ce4f80ef2a33fa0de165608707942494dae
Sprazzeus <sprazzeus@gmail.com>
2016-09-22 01:44:46 +0800

    completed pdp

--- src/pdp.js
+++ src/pdp.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react'
 import {Link, hashHistory} from 'react-router'
-import $ from 'jquery'
+import './pdp.css'
 import dataSample from './products.json'
 
 class Product extends Component {
-  componentDidMount() {
-  this.setState({
-    product: findCurrentProduct(this.props.params)
-  })
-}
+  constructor(props) {
+      super(props);
+      this._findProduct = this._findProduct.bind(this);
+      this.state = {
+        productId: this.props.params.productId,
+        name: '',
+        price: '',
+        measurement: '',
+        desc: '',
+        image: ''
+      }
+  }
 
-render() {
-  return (
-    <div>
-      <h2>HI
-      </h2>
-    </div>
-  )
-}
-}
+  componentWillMount() {
+    this._findProduct()
+  }
 
-// {sampleData.products.find(function(product){
-//   return product.image
-// })}
+  _findProduct() {
+    var currentProduct = this.state.productId
+    var result = dataSample.products.find(function(product) {
+      return ( product.image === (currentProduct + '.jpg') )
+    })
+    console.log('CURRENT PRODUCT NAME: ' + result.name)
+    console.log(this.state.name)
+    this.setState({
+      name: result.name,
+      price: result.price,
+      measurement: result.measurement,
+      desc: result.desc,
+      image: result.image
+    })
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="product-detail">
+          <h2>{this.state.name}</h2>
+
+          <div className="details">
+            <div className="details-image">
+              <img src={require('./img/' + this.state.image)} alt="product" className="details-image__image" />
+            </div>
+
+            <div className="details-text">
+              <h2 className="details-text__measurement">1L</h2>
+              <h1 className="details-text__price">${this.state.price}</h1>
+              <p className="details-text__desc">
+                {this.state.desc}
+              </p>
+              <button className="details-text__add-to-cart-button"><h1>Add To Cart</h1></button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export { Product }
