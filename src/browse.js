bd69d9f93baf2e1c4a921fe4c866d186949e8f59
Sprazzeus <sprazzeus@gmail.com>
2016-09-21 16:44:09 +0800

    refactored ProductFilterContainer and ProductFilterList. Data set in state.

--- src/browse.js
+++ src/browse.js
@@ -1,73 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 import './browse.css';
 import dataSample from './products.json';
 
 const Browse = React.createClass({
-  render () {
+  render() {
     return (
-      <div className="main">
-        <ProductFilterBox data={dataSample} />
+      <div className='main'>
+        <ProductFilterContainer />
         <ProductList data={dataSample} />
       </div>
     )
   }
 })
 
-const ProductFilterBox = React.createClass({
-  render () {
+class ProductFilterContainer extends Component {
+  constructor() {
+    super();
+    this.state = { productFilters: [] }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: "https://raw.githubusercontent.com/sprazzeus/react-products-exercise/development/src/products.json",
+      dataType: 'json',
+      success: function(data) {
+        this.setState({productFilters: data.filters});
+      }.bind(this)
+    });
+  }
+
+  render() {
     return (
-      <div className="product-filter-box">
-        <ProductFilterList data={this.props.data} />
+      <div className='product-filter-container'>
+        <ProductFilterList productFilters={this.state.productFilters} />
       </div>
     )
   }
-})
+}
 
-const ProductFilterList = React.createClass({
-  render: function() {
-    var productFilterCategory = this.props.data.filters.map(function(filter) {
+class ProductFilterList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <ul>{this.props.productFilters.map(function(productFilter) {
+      // render each filter category
       return (
-        <ul>
-          <h4>{filter.name}</h4>
-          {filter.values.map(function(value) {
+        <li>
+          <h4>{productFilter.name}</h4>
+          {productFilter.values.map(function(value) {
+            // render each filter within category
             return (
               <li>
-                <label for={"option" + value}>
-                  <input id={"option" + value} type="checkbox" name="field" value="option"/>{value}
+                <label for={'option' + value}>
+                  <input id={'option' + value} type='checkbox' name='field' value='option'/>{value}
                 </label>
               </li>
             )
           })}
-        </ul>
-      );
-    });
-    return (
-      <form className="productFilterList">
-        {productFilterCategory}
-      </form>
-    );
+        </li>
+      )
+    })}</ul>
   }
-});
+}
 
 const ProductList = React.createClass({
-  render () {
+  render() {
     var productCard = this.props.data.products.map(function(product) {
       return (
-        <div className="product-list__card">
-          <div className="product-info">
-            <img src={require('./img/' + product.image)} alt="product" />
-            <span className="product-info__name">{product.name}</span>
-            <span className="product-info__measurement">{product.measurement}</span>
+        <div className='product-list__card'>
+          <div className='product-info'>
+            <img src={require('./img/' + product.image)} alt='product' />
+            <span className='product-info__name'>{product.name}</span>
+            <span className='product-info__measurement'>{product.measurement}</span>
           </div>
-          <div className="product-cta">
-            <span className="product-cta__price"><strong>${product.price}</strong></span>
-            <button className="product-cta__add-to-cart-button">Add to Cart</button>
+          <div className='product-cta'>
+            <span className='product-cta__price'><strong>${product.price}</strong></span>
+            <button className='product-cta__add-to-cart-button'>Add to Cart</button>
           </div>
         </div>
       )
     })
     return (
-      <div className="product-list">
+      <div className='product-list'>
         {productCard}
       </div>
     )
@@ -76,4 +93,4 @@ const ProductList = React.createClass({
 
 
 
-export { Browse, ProductFilterBox, ProductFilterList, ProductList }
+export { Browse, ProductFilterContainer, ProductFilterList, ProductList }
