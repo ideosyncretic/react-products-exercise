efcb9729576728e96cd6043f0f99f91c92b7cc0f
Sprazzeus <sprazzeus@gmail.com>
2016-09-22 21:56:26 +0800

    changed browserHistory back to hashHistory due to incompatibility with GH-pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 import {App, Browse, Cart, Product} from './App';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Browse}/>
-      <Route path="/product/:productId" component={Product}/>
+      <Route path="product/:productId" component={Product}/>
       <Route path="cart" component={Cart}/>
     </Route>
   </Router>,
